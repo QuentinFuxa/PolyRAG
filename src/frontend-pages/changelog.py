adb7cbef8e806e12e38382bac50e68809315e2e6
Quentin Fuxa <quentin.fuxa@gmail.com>
2025-06-16 11:27:11 +0200

    update changelog date

--- src/frontend-pages/changelog.py
+++ src/frontend-pages/changelog.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
 st.header("Nouveautés :")
-st.subheader("12/06/2025")
+st.subheader("16/06/2025")
 st.markdown("""
-- Correction de bugs mineurs
-- Correction de l'upload de documents PDF
+- Déploiement aux utilisateurs
 """)
